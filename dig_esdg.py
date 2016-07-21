f163a77ab12c7142e58b83eb9a63db8d6199bb48
ZwEin <zwein27@gmail.com>
2016-07-21 11:45:12 -0400

    load websites

--- dig_esdg.py
+++ dig_esdg.py
@@ -2,9 +2,8 @@
 # @Author: ZwEin
 # @Date:   2016-07-21 11:12:02
 # @Last Modified by:   ZwEin
-# @Last Modified time: 2016-07-21 11:24:04
+# @Last Modified time: 2016-07-21 11:44:56
 
-import arg_parser
 import urllib3
 import sys
 from elasticsearch import Elasticsearch
@@ -13,13 +12,90 @@ import getopt
 
 urllib3.disable_warnings()
 
+######################################################################
+#   Constant
+######################################################################
+
+
+######################################################################
+#   Query
+######################################################################
+
+sites_query = {
+    "aggs": {
+        "by-posttime": {
+            "filter": {
+                "bool": {
+                    "must": [
+                        {
+                            "term": {
+                                "version": "2.0"
+                            }
+                        },
+                        {
+                            "exists": {
+                                "field": "extractions.text"
+                            }
+                        }
+                    ]
+                }
+            },
+            "aggs": {
+                "posttime": {
+                    "terms": {
+                        "field": "extractions.text.attribs.target",
+                        "size": 1000
+                    }
+                }
+            }
+        }
+    },
+    "size": 0
+}
+
+search_query = {
+    "query": {
+        "filtered": {
+            "filter": {
+                "exists": {
+                    "field": "extractions.text"
+                }
+            },
+            "query": {
+                "match": {
+                    "extractions.text.results": "blvd"
+                }
+            }
+        }
+    },
+    "_source": [
+        "extractions.text.results"
+    ],
+    "size": 500
+}
+
+
+
+######################################################################
+#   Main Class
+######################################################################
 
 class DIGESDG(object):
 
     def __init__(self, token):
-        self.username, self.password = token.split(':')
+        # self.username, self.password = token.split(':')
+        self.cdr = 'https://' + token + '@els.istresearch.com:19200/memex-domains'
+        self.es = Elasticsearch(self.cdr)
+
+    def load_sites(self):
+        buckets = self.es.search(index='escorts', body=sites_query)['aggregations']['by-posttime']['posttime']['buckets']
+        sites = map(lambda x: x['key'], buckets)
+        return sites
+
 
     def generate(self):
+        print self.load_sites()
+        # print ", ".join(sites)
 
 
 
@@ -32,6 +108,8 @@ if __name__ == '__main__':
 
     args = arg_parser.parse_args()
 
-    print args.token
+    dg = DIGESDG(args.token)     
+
+    print dg.generate()
 
 
