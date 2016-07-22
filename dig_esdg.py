a8dc28774d07ca1c5b40fd852d7204b61daf629c
ZwEin <zwein27@gmail.com>
2016-07-21 22:00:38 -0400

    add load_data_by_site

--- dig_esdg.py
+++ dig_esdg.py
@@ -2,7 +2,7 @@
 # @Author: ZwEin
 # @Date:   2016-07-21 11:12:02
 # @Last Modified by:   ZwEin
-# @Last Modified time: 2016-07-21 11:48:25
+# @Last Modified time: 2016-07-21 21:59:58
 
 import urllib3
 import sys
@@ -57,8 +57,24 @@ search_query = {
     "query": {
         "filtered": {
             "filter": {
-                "exists": {
-                    "field": "extractions.text"
+                "bool": {
+                    "must": [
+                        {
+                            "term": {
+                                "extractions.text.attribs.target": ""
+                            }
+                        },
+                        {
+                            "exists": {
+                                "field": "extractions"
+                            }
+                        },
+                        {
+                            "exists": {
+                                "field": "extractions.text.results"
+                            }
+                        }
+                    ]
                 }
             },
             "query": {
@@ -71,7 +87,7 @@ search_query = {
     "_source": [
         "extractions.text.results"
     ],
-    "size": 500
+    "size": 1
 }
 
 
@@ -85,7 +101,7 @@ class DIGESDG(object):
     def __init__(self, token):
         # self.username, self.password = token.split(':')
         self.cdr = 'https://' + token + '@cdr-es.istresearch.com:9200/memex-domains'
-        self.es = Elasticsearch(self.cdr)
+        self.es = Elasticsearch([self.cdr])
 
     def load_sites(self):
         buckets = self.es.search(index='escorts', body=sites_query)['aggregations']['by-posttime']['posttime']['buckets']
@@ -93,13 +109,32 @@ class DIGESDG(object):
         return sites
 
     def load_data_by_site(self, site_name):
-        pass
+        try:
+            # print site_name
+            search_query['query']['filtered']['filter']['bool']['must'][0]['term']['extractions.text.attribs.target'] = site_name
+            return search_query['query']
+            
+            # print json.dumps(search_query, indent=4)
+            # buckets = self.es.search(index='escorts', body=search_query)['hits']['hits']
+        except Exception as e:
+            raise Exception('site_name is incorrect')
+        return 'ss'
+        ans = []
+        for bucket in buckets:
+            try:
+                text = bucket['_source']['extractions']['text']['results']
+                if not isinstance(text, basestring):
+                    text = ' '.join(text)
+            except Exception as e:
+                continue
+            else:
+                ans.append(text)
+        return ans
 
     def generate(self):
-        site_name = 'liveescortreviews'
-
-        print self.load_sites()
-        # print self.load_data_by_site(site_name)
+        site_name = 'backpage'
+        # print self.load_sites()
+        return self.load_data_by_site(site_name)
         # print ", ".join(sites)
 
 
